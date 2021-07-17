5d2601332a9d2b74322e9c2afbeadc220fdc239b
rabindra sharma <sharmarabindra29@gmail.com>
2021-07-17 22:39:51 +0530

    first commited

---
+++ src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+    private id: number;
+    private sku: string;
+    private name: string;
+    private description: string;
+    private unitPrice: number;
+    private active: boolean;
+    private unitsInStock: number;
+    private dateCreated: Date;
+    private lastUpdated: Date;
+    
+}
